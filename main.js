4ebe058dcc142c90073211e1b41a2bd43a807fca
Ariel <arielluquer@hotmail.com>
2024-02-15 17:35:06 -0600

    progress 17:35 Feb 15, 2024

--- main.js
+++ main.js
@@ -1,3 +1,44 @@
+// const fs = require('fs');
+// const NodeXls = require('node-xls');
+// const fs = 'fs';
+// require([fs], function(result) {
+//     fs = result;
+// })
+// const NodeXls = 'node-xls';
+// require([NodeXls], function(result) {
+//     NodeXls = result;
+// })
+
+//const XLSX = require("xlsx");
+const XLSX = require(["xlsx"]);
+const file = XLSX.readFile("record.xlsx");
+console.log(file);
+
+//console.log(readXlsxFile("record.xlsx"));
+// test.addEventListener('change', () => {
+//     console.log(test.files);
+//     readXlsxFile(test.files[0]).then((rows) => {
+//         console.log(rows);
+//     });
+// });
+
+// readXlsxFile("record.xlsx").then(response => {
+//     console.log(response);
+// }).catch(e => {
+//     console.log(e);
+// });
+
+
+// const XLSX = require("xlsx");
+// const records = XLSX.readFile("record.xlsx");
+// const record = records.Sheets["Sheet1"];
+
+// const newRecord = ["Jim", "Professional", 24, 53];
+
+// const info = XLSX.utils.sheet_to_json(record);
+// console.log(info);
+
+/*
 const Categorys = Object.freeze({
     Student: "Student",
     Amateur: "Amateur",
@@ -13,24 +54,31 @@ class Record
     constructor() {
         this.readInput();
         this.highestScore = this.findHighestScore();
-        this.generateRecord();
-        console.log(this.data);
+        this.generateSheetsRecord();
     }
 
     findHighestScore() {
         return this.firstScore > this.secondScore ? this.firstScore : this.secondScore;
     }
 
+    // Construct a map from the input fields
     readInput() {
         const inputs = document.getElementsByClassName("input");
         for(const input in inputs) {
-            this.data.set(inputs[input].id, inputs[input].value);
+            const id = inputs[input].id ? inputs[input].id : '';
+            const value = inputs[input].value ? inputs[input].value : '';
+            this.data.set(id, value);
         }
     }
     
-    // function to actually generate a record on the excel sheet (or at least one that is in the right format)
-    generateRecord() {
-        
+    // function which generates a record on the excel sheet (or at least one that is in the right format)
+    // 14-02-2024 -> Couldn't figure out how to accomplish this without creating a new array (just using this.sheetsRecord.push(value))
+    generateXlsRecord() {
+        const record = [];
+        function addToArray(value, key, map) {
+            record.push(value);
+        }
+        this.data.forEach(addToArray);
     }
 }
 
@@ -53,10 +101,15 @@ function resetForm() {
     
 }
 
+document.getElementById("submit").addEventListener("click", function() {
+    record = new Record();
+    resetForm();
+});
+
 function main() {
     generateCategories();
     generateTime();
-    record = new Record();
 }
 
 main();
+*/
